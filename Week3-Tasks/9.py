c475d304aa9cbe4ffc12ce85e9275e1d73c7f573
CharlesDavison <1927458@fife.ac.uk>
2025-09-16 14:45:27 +0100

    Added Semicolons to 9.py
    I thought it was funny.

--- Week3-Tasks/9.py
+++ Week3-Tasks/9.py
@@ -3,10 +3,10 @@ import datetime;
 dayOfWeek = datetime.datetime.today().weekday();
 
 if dayOfWeek in range(3, 4):
-        print("Soon it is the weekend!")
+        print("Soon it is the weekend!");
     
 elif dayOfWeek in range(5, 6):
-        print("It is the weekend!")
+        print("It is the weekend!");
 
 elif dayOfWeek in range(0, 2):
-        print("Looking forward to the weekend!")
+        print("Looking forward to the weekend!");
